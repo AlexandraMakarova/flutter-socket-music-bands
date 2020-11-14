f20f42bbff51b04dad99ddcd3992aea3e0e01b88
Alexandra Makarova <Alex@MacBook-Pro-de-mac.local>
2020-11-14 09:41:53 -0300

    Finished backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,48 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('RHCP'));
+bands.addBand(new Band('La Vela Puerca'));
+bands.addBand(new Band('Nirvana'));
+bands.addBand(new Band('Ska-P'));
+
+
 
 //Sockets message
 io.on('connection', client => {
-    console.log('Client conected');
+    client.emit('active-bands', bands.getBands());
 
-    client.on('disconnect', () => { console.log('Client desconected') });
+    client.on('disconnect', () => {  });
 
     client.on('message', (payload) => {
         console.log('Mensaje!', payload);
-
         io.emit('server_emited_message', {admin: 'New Message'});
     });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band (payload.name)
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+
+    // client.on('emit-message', (payload) => {
+    //     // io.emit('new-flutter-message', payload); //emit for all
+    //     client.broadcast.emit('new-flutter-message', payload); //emit for all, except itself
+    // });
+
+
   });
